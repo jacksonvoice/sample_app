a44e7e2c40d6cdfe78f2da3371d10a96a11d9064
Daniel Jackson <jacksonvoice@yahoo.com>
2014-07-10 15:58:18 -0700

    add ssl in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -10,6 +10,11 @@ Rails.application.configure do
   # Rake tasks automatically ignore this option for performance.
   config.eager_load = true
 
+
+  #force all access to the app over SSL, use strict-transport-secutrity,
+  #and use secure cookies.
+  config.force_ssl = true
+
   # Full error reports are disabled and caching is turned on.
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
